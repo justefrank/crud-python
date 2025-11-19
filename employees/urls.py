a9634a03403f91e588bd171965a7f9facdaaca57
juste frank <justefrank9@gmail.com>
2025-11-19 02:02:19 +0000

    Mise à jour des URLs pour inclure les routes ajouter/modifier/supprimer

--- employees/urls.py
+++ employees/urls.py
@@ -4,4 +4,7 @@ from .import views
 
 urlpatterns = [
     path("", views.liste_employes, name='liste_employes'),
+    path("ajouter/", views.ajouter_employe, name='ajouter_employe'),
+    path("modifier/<int:id>", views.modifier_employe, name='modifier_employe'),
+    path("supprimer/<int:id>/", views.supprimer_employe, name='supprimer_employe')
 ]
